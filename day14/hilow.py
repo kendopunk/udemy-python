75b0eee9ed8bc5b2b4e17ce46ff573417fb7b793
Mark Fehrenbacher <mark.fehrenbacher@carrier.com>
2022-04-08 08:59:30 -0400

    Day 14 - hi/low game

---
+++ day14/hilow.py
@@ -0,0 +1,58 @@
+# hilow.py
+from random import choice
+import os
+
+from art import logo, vs
+from game_data import data
+
+# control variables
+score = 0
+game_on = True
+
+
+def descriptor(mapElement):
+    return ("%s, a %s from %s" %
+            (mapElement["name"], mapElement["description"], mapElement["country"]))
+
+
+def comparison_statement(prefix, mapElement):
+    desc = descriptor(mapElement)
+    if prefix == 'A':
+        print(f"Compare A: {desc}")
+    if prefix == 'B':
+        print(f"Against B: {desc}")
+
+
+def increment_score(score):
+    return score + 1
+
+
+def is_correct_choice(guess, a_followers, b_followers):
+    if guess == 'A' and a_followers > b_followers:
+        return True
+    if guess == 'B' and b_followers > a_followers:
+        return True
+    return False
+
+
+while game_on:
+    a = choice(data)
+    b = choice(data)
+    print(logo)
+    print(f"Your score is {score}.")
+    comparison_statement('A', a)
+    print(vs)
+    comparison_statement('B', b)
+
+    guess = 'X'
+    while not guess == 'A' and not guess == 'B':
+        guess = input("Who has more followers? (A / B): ")
+
+    if is_correct_choice(guess, a["follower_count"], b["follower_count"]):
+        score = increment_score(score)
+        os.system('clear')
+    else:
+        game_on = False
+        print((50 * "="))
+        print(f"Game over.  Your score is {score}.")
+        print((50 * "="))
