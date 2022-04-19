79112b1e5882ba536e9ef292fc89e10c18581b80
Mark Fehrenbacher <mark.fehrenbacher@carrier.com>
2022-04-19 11:11:33 -0400

    Day 25 - pandas

---
+++ day25/main.py
@@ -0,0 +1,11 @@
+# import pandas as p
+
+# file = "weather_data.csv"
+
+# df = p.read_csv(file)
+
+# monday = df[df.day == "Wednesday"]
+# print(int(monday.temp) * 9/5 + 32)
+
+# x = (59 - 32) * 5/9
+# print(x)
---
+++ day25/squirrel.py
@@ -0,0 +1,38 @@
+# 2018 Squirrel Data w/ pandas
+import pandas as p
+
+# shorter CSV file name
+FILE = "squirrel_data.csv"
+PFC = "Primary Fur Color"
+ACCEPTABLE_COLORS = ['Gray', 'Cinnamon', 'Black']
+
+
+def normalize_fur_color(color):
+    if color == 'Gray':
+        return 'grey'
+    elif color == 'Cinnamon':
+        return 'red'
+    else:
+        return color.lower()
+
+
+# read the csv
+df = p.read_csv(FILE)
+
+# drop missing / null / NaN, etc from "Primary Fur Color" column
+df.dropna(subset=[PFC], inplace=True)
+
+# filter on acceptable colors
+color_filter = df[PFC].isin(ACCEPTABLE_COLORS)
+df = df[color_filter]
+
+# apply the converter(s)
+converters = {
+    PFC: lambda x: normalize_fur_color(x)
+}
+df[PFC] = df[PFC].apply(converters[PFC])
+
+# aggregate by color, change column names, convert to CSV
+agg_by_color = df.groupby(PFC).size().reset_index(name='Count')
+agg_by_color.columns = ['Fur Color', 'Count']
+agg_by_color.to_csv("squirrel_count_by_color.csv")
